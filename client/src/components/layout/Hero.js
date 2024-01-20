7e4e8dd42f6380b39fcf7381b8bf791b45085bcc
Dinethra Sulakshana <dinethrasulakshana@gmail.com>
2024-01-20 23:50:01 +0530

    Created login page

--- client/src/components/layout/Hero.js
+++ client/src/components/layout/Hero.js
@@ -16,11 +16,11 @@ export default function Hero() {
                 Pizza is the missing piece thet makes every day complete, a simple yet delicious joy in life.
             </p>
             <div className="flex gap-4 text-sm">
-                <button className="bg-primary uppercase flex items-center gap-2 text-white px-8 py-2 rounded-full">
+                <button className="justify-center bg-primary uppercase flex items-center gap-2 text-white px-8 py-2 rounded-full">
                     Order now
                     <Right />
                 </button>
-                <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+                <button className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold">
                     Learn more
                     <Right />
                 </button>
